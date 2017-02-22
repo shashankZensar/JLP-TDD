2a1b063663912e66ec5a9d6e9bd9add04545f63e
Shashank Dekate <shashankdekate@Shashanks-MacBook-Air.local>
2017-02-23 00:58:33 +0530

    constant file added

---
+++ DishWasherApp/Resource/Constant.swift
@@ -0,0 +1,57 @@
+//
+//  Constant.swift
+//  DishWasherApp
+//
+//  Created by Shashank Dekate on 22/02/17.
+//  Copyright © 2017 Shashank Dekate. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - StoryBoard indentifier
+
+let kProduct_InfoCell = "productInfoCell"
+let kImageColectionView = "imageColectionView"
+
+
+
+// MARK: -  Json Parse detail Constant
+
+let kTitle = "title"
+let kMedia = "media"
+let kImages = "images"
+let kUrls = "urls"
+let kPrice = "price"
+let kNow = "now"
+let kDetails = "details"
+let kProductInformation = "productInformation"
+let kDisplaySpecialOffer = "displaySpecialOffer"
+let kCode = "code"
+let kAdditionalServices = "additionalServices"
+let kIncludedServices = "IncludedServices"
+let kFeatures = "features"
+let kAttributes = "attributes"
+let kName = "name"
+let kValue = "value"
+let kHttps = "https:"
+let kProducts = "products"
+let kproductId = "productId"
+let kImage = "image"
+let kWas = "was"
+let kThen1 = "then1"
+let kThen2 = "then2"
+let kUom = "uom"
+let kCurrency = "currency"
+
+
+// MARK: - Network Api
+
+let kProductGridAPI = "https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20"
+
+
+let kDishWashers = "Dishwashers"
+let kProductInformationLbl = "Product Information"
+let kProductSpecificationLbl = "Product Specification"
+
+
